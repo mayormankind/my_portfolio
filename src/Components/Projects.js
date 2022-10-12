065f9c1a3dcea96fa305b66db76d193c95c63051
Makinde Mayowa <mayowamakinde23@gmail.com>
2022-10-12 01:37:58 +0100

    Update Projects.js

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -7,7 +7,7 @@ function Projects() {
     const [isBigScreen] = useMediaQuery('(min-width:600px)');
     const projectList = [
         {id:'1',projectName:'Feedback Manga',projectDetails:'A yearBook with a schedular just for you.',projectImage:'https://res.cloudinary.com/dcesze7l8/image/upload/v1665533472/portfolio/p1_yyfza8.png',pref:'https://lightgram-mod.vercel.app'},
-        {id:'2',projectName:'HoistMe',projectDetails:'A CMS to create your own free business portfolio',projectImage:'/p2.png',pref:'https://lightgram-mod.vercel.app'},
+        {id:'2',projectName:'HoistMe',projectDetails:'A CMS to create your own free business portfolio',projectImage:'https://res.cloudinary.com/dcesze7l8/image/upload/v1665533483/portfolio/p3_oeeqme.png',pref:'https://lightgram-mod.vercel.app'},
         {id:'3',projectName:'Lightgram LoginPage',projectDetails:'A central web app with for a community.',projectImage:'https://res.cloudinary.com/dcesze7l8/image/upload/v1665533483/portfolio/p3_oeeqme.png',pref:'https://lightgram-mod.vercel.app'},
         {id:'4',projectName:'Lightgram WebApp',projectDetails:'A central web app with for a community.',projectImage:'https://res.cloudinary.com/dcesze7l8/image/upload/v1665533485/portfolio/p4_wz6gxs.png',pref:'https://lightgram-app.vercel.app'},
         {id:'5',projectName:"Mankind's Portfolio",projectDetails:'my personal potfolio made with react and chakra UI',projectImage:'/https://res.cloudinary.com/dcesze7l8/image/upload/v1665533472/portfolio/p5_m8xwvj.png',pref:'https://my-portfolio-delta-lac.vercel.app/'},
