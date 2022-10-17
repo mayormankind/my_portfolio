a34a37a24bc5cfc4ec05e0bb2e4200f07cd0ccd7
Makinde Mayowa <mayowamakinde23@gmail.com>
2022-10-17 04:28:44 +0100

    Update ProjectCard.js

--- src/LinkSrc/ProjectCard.js
+++ src/LinkSrc/ProjectCard.js
@@ -3,8 +3,17 @@ import React,{useState} from 'react';
 import {FaForward} from 'react-icons/fa'
 
 function ProjectCard(props) {
+// const [readMore,setReadmore] = useState(false);
 const [readMore,setReadmore] = useState(false);
 
+const ReadModal = (props)=>{
+    return(
+        <Flex bg={'rgba(0,0,0,1)'} p='0 10px' w='100%' h='100%' color='white' position={'absolute'} bottom='0' textAlign={'center'} justify={'center'} flexDir={'column'} zIndex='1'>
+            <Text fontSize={'15px'}>{props.det}</Text>
+            <Link href={props.href} onClick={()=>setReadmore(false)} mt='10px'><Text as={'span'}>Go to Site</Text></Link>
+        </Flex>        
+    )
+}
   return (
     <Box key={props.id} mb={'20px'} w='100%' onClick={(e)=>setReadmore(true)}>
         <Flex flexDir={'column'} position='relative'>
@@ -15,14 +24,6 @@ const [readMore,setReadmore] = useState(false);
         </Flex>
             <Text textAlign={'center'} fontSize='20px' fontFamily={'monospace'}>{props.name}</Text>
     </Box>
-    )
-}
-export const ReadModal = (props)=>{
-    return(
-        <Flex bg={'rgba(0,0,0,1)'} p='0 10px' w='100%' h='100%' color='white' position={'absolute'} bottom='0' textAlign={'center'} justify={'center'} flexDir={'column'} zIndex='1'>
-            <Text fontSize={'15px'}>{props.det}</Text>
-            <Link href={props.href} onClick={()=>setReadmore(false)} mt='10px'><Text as={'span'}>Go to Site</Text></Link>
-        </Flex>        
     )
 }
 
