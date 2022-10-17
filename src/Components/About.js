317b8a7e84d4c48d1fd9f54c55eeecd1bd9ad8fe
Makinde Mayowa <mayowamakinde23@gmail.com>
2022-10-17 04:05:39 +0100

    Update About.js

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box, Flex, Text, VStack, Circle, Stack, Image, Spacer, useMediaQuery, Button, useColorMode } from '@chakra-ui/react';
+import { Box, Flex, Text, Stack, Image, useMediaQuery, Button, useColorMode } from '@chakra-ui/react';
 
 export default function About() {
   const [isBigScreen] = useMediaQuery('(min-width:600px)');
@@ -11,14 +11,11 @@ export default function About() {
           <Box align='flex-start' w={isBigScreen?'70%':'100%'}>
             <Text fontSize={isBigScreen ? '3xl' :'2xl'}>Hello there, I'm </Text>
             <Text fontSize={isBigScreen ? '5xl' :'3xl'} fontWeight='bold'>Makinde Mayowa</Text>
-            <Text>I am an entusiastic frontend developer with lots of vibes. You get what you want only if you give what you have. Lol</Text>
+            <Text>I'm an entusiastic frontend developer with lots of vibes. I specialize in making catchy web apps that you might just want to sleep on. I really do love classic designs and interfaces and that makes me want to work on myself. You get what you want only if you give what you have. Guess i have the time that's why i'm giving it. Lol!!</Text>
             <Button w={isBigScreen?'80%':'100%'} mt={'10px'} as='a' href='mailto:mayowamakinde23@gmail.com'>Hire Me</Button>
-            <Button as='a' target={"_blank"} w={isBigScreen?'80%':'100%'} mt={'10px'} href="mailto:mayowamakinde23@gmail.com">Hire Me</Button>
           </Box>
           <Image alt='Makinde Mayowa' src='/mayowa.jpg' objectFit={'contain'} boxSize={'250'} boxShadow='lg' alignSelf={'center'} mt={isBigScreen?'0':'10px'} borderRadius='50%' border={isDark?'10px solid silver':'10px solid black'}/>
         </Flex>
       </Stack>
   )
 }
-
-// export default About;
