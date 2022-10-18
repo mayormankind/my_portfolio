4d928b55870dab338c6d5262ca37a5e3f3ceb2c9
Makinde Mayowa <mayowamakinde23@gmail.com>
2022-10-19 00:47:05 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { extendTheme, ColorModeScript, ChakraProvider } from '@chakra-ui/react'
+import { extendTheme, ColorModeScript, ChakraProvider } from '@chakra-ui/react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
