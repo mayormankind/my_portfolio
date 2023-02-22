f71cb0f21031a9cafdb4e526b2bd14686b4b979a
Makinde Mayowa <mayowamakinde23@gmail.com>
2023-02-22 11:17:32 +0100

    update About.js

--- src/chakra/src/Components/About.js
+++ src/chakra/src/Components/About.js
@@ -10,7 +10,6 @@ export default function About() {
           <Box w={{sm:'60%',base:'100%'}}>
             <Text fontSize={{sm:'3xl',base:'2xl'}}>Hello there, I'm </Text>
             <Text fontSize={{sm:'5xl',base:'3xl'}} fontWeight='bold'>Makinde Mayowa</Text>
-            <Text textAlign={'justify'}>I'm an entusiastic frontend developer with lots of vibes 🚀🚀. I specialize in making catchy web apps that you might just want to sleep on. I really do love classic designs and interfaces and that makes me want to work on myself. You get what you want only if you give what you have. Guess i have the time that's why i'm giving it. Lol!!😂😂😂😂</Text>
             <Text textAlign={'justify'}>, a skilled and experienced web developer with a passion for crafting innovative and visually stunning websites. With 3 years of experience in the industry, I have honed my skills in developing cutting-edge websites that not only look great but also function seamlessly. With a keen eye for detail and a commitment to excellence, I work closely with clients to understand their needs and create custom solutions that exceed their expectations. Whether you need a simple website or a complex web application, I have the expertise and creativity to bring your vision to life. Check out my portfolio to see some of the incredible projects I have worked on.</Text>
             <Button bg={isDark?'white':'rgb(30,30,30)'} color={isDark?'black':'white'} w={{sm:'80%',base:'100%'}} mt={'10px'} as='a' href='mailto:mayowamakinde23@gmail.com'>Mail Me</Button>
           </Box>
@@ -19,3 +18,5 @@ export default function About() {
       </Stack>
   )
 }
+
+  <Text textAlign={'justify'}>I'm an entusiastic frontend developer with lots of vibes 🚀🚀. I specialize in making catchy web apps that you might just want to sleep on. I really do love classic designs and interfaces and that makes me want to work on myself. You get what you want only if you give what you have. Guess i have the time that's why i'm giving it. Lol!!😂😂😂😂</Text>
