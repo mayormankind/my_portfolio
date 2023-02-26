ac8ab80ef36809b08aa9e053c1a39ad8387a9344
Makinde Mayowa <mayowamakinde23@gmail.com>
2023-02-26 07:06:47 +0100

    update Container.js

--- src/Components/Container.js
+++ src/Components/Container.js
@@ -6,7 +6,7 @@ export default function Container({children}) {
   const {colorMode} = useColorMode();
   const isDark = colorMode=='dark';
   return (
-    <Box className='bgg' h='100%' w={'100%'} overflow='hidden' bgGradient={isDark?'linear(to-tr,black,red,black)':'linear(to-tr,white,lightgray,gray)'}>
+    <Box className='bgg' h='100%' w={'100%'} overflow='hidden' bg={isDark? '#262626' : '#FAFAFA'}>
         <Box maxW={'1200px'} mx='auto' w={'100%'}>
           <Header/>
           <Box position={'relative'}>
